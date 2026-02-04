6e9f0337546c7a9481bfde9b1df624de751ce748
Alex Lee <lee3742@purdue.edu>
2026-02-04 05:04:20 +0000

    Finish python script for ading has HP columns

--- utils/identify_hp_customers.py
+++ utils/identify_hp_customers.py
@@ -4,6 +4,13 @@ import io
 import polars as pl
 from cloudpathlib import S3Path
 
+IN_HVAC_COLUMNS = ["in.hvac_cooling_type", "in.hvac_heating_type"]
+
+UPGRADE_HVAC_COLUMNS = [
+    "upgrade.hvac_cooling_efficiency",
+    "upgrade.hvac_heating_efficiency",
+]
+
 
 def get_upgrade_ids(data_path: str, release: str, state: str) -> list[str]:
     """Test that the resstock data is downloaded to the correct path."""
@@ -23,6 +30,55 @@ def get_upgrade_ids(data_path: str, release: str, state: str) -> list[str]:
     ]
     print(f"Upgrade ids: {upgrade_ids}")
     return upgrade_ids
+
+
+def identify_hp_customers(metadata_path: S3Path, upgrade_id: str):
+    metadata_bytes = metadata_path.read_bytes()
+    metadata_df = pl.read_parquet(io.BytesIO(metadata_bytes))
+
+    if upgrade_id == "00":
+        hvac_cooling_type = metadata_df[IN_HVAC_COLUMNS[0]]
+        hvac_heating_type = metadata_df[IN_HVAC_COLUMNS[1]]
+        hp_customers = (
+            hvac_cooling_type.str.contains("Heat Pump", literal=True)
+            & hvac_heating_type.str.contains("Heat Pump", literal=True)
+        ).fill_null(False)
+    else:
+        in_hvac_cooling_type = metadata_df[IN_HVAC_COLUMNS[0]]
+        in_hvac_heating_type = metadata_df[IN_HVAC_COLUMNS[1]]
+        upgrade_hvac_cooling_type = metadata_df[UPGRADE_HVAC_COLUMNS[0]]
+        upgrade_hvac_heating_type = metadata_df[UPGRADE_HVAC_COLUMNS[1]]
+        # Row-wise OR: upgrade path is HP *or* in.path is HP
+        upgrade_is_hp = upgrade_hvac_cooling_type.str.contains(
+            "Heat Pump", literal=True
+        ) & (
+            upgrade_hvac_heating_type.str.contains("MSHP", literal=True)
+            | upgrade_hvac_heating_type.str.contains("ASHP", literal=True)
+            | upgrade_hvac_heating_type.str.contains("GSHP", literal=True)
+        )
+        in_is_hp = in_hvac_cooling_type.str.contains("Heat Pump", literal=True) & (
+            in_hvac_heating_type.str.contains("Heat Pump", literal=True)
+        )
+        hp_customers = (upgrade_is_hp | in_is_hp).fill_null(False)
+    metadata_df = metadata_df.with_columns(
+        hp_customers.alias("postprocess_group.has_hp")
+    )
+    """print(f"Upgrade id: {upgrade_id}")
+    print(f"Number of HP customers: {metadata_df['postprocess_group.has_hp'].sum()}")
+
+    non_hp_customers = metadata_df.filter(~pl.col("postprocess_group.has_hp"))
+    non_hp_customer_in_values = non_hp_customers["in.hvac_heating_type_and_fuel"]
+    print("Number of total customers: ", len(metadata_df))
+    print("Number of non-HP customers: ", len(non_hp_customers))
+    print(f"Non-HP customer values: {non_hp_customer_in_values.unique()}")
+    if upgrade_id != "00":
+        non_hp_customer_upgrade_values = non_hp_customers[
+            "upgrade.hvac_heating_efficiency"
+        ]
+        print(
+            f"Non-HP customer upgrade values: {non_hp_customer_upgrade_values.unique()}"
+        )"""
+    return metadata_df
 
 
 def add_has_HP_column(data_path: str, release: str, state: str):
@@ -48,6 +104,14 @@ def add_has_HP_column(data_path: str, release: str, state: str):
             pl.read_parquet(io.BytesIO(metadata_bytes)).select("bldg_id").unique()
         )
         print(f"Number of metadata bldg ids: {len(metadata_bldg_ids)}")
+        sb_metadata_df = identify_hp_customers(metadata_path, upgrade_id)
+        sb_metadata_df.write_parquet(
+            release_dir
+            / "metadata"
+            / f"state={state}"
+            / f"upgrade={upgrade_id}"
+            / "metadata-sb.parquet"
+        )
 
 
 def main():
@@ -73,5 +137,5 @@ if __name__ == "__main__":
     add_has_HP_column(
         data_path="s3://data.sb/nrel/resstock/",
         release="res_2024_amy2018_2",
-        state="NY",
+        state="RI",
     )
