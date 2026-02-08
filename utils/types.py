e21cb130ea862c842649af96bed5fd2168979518
Alex Lee <lee3742@purdue.edu>
2026-02-08 03:46:10 +0000

    Simplify tariff_key assigning function

--- utils/types.py
+++ utils/types.py
@@ -5,12 +5,16 @@ class SB_scenario:
     analysis_type: Literal["default", "seasonal", "class_specific_seasonal"]
     analysis_year: int
 
-    def __init__(self, analysis_type: Literal["default", "seasonal", "class_specific_seasonal"], analysis_year: int):
+    def __init__(
+        self,
+        analysis_type: Literal["default", "seasonal", "class_specific_seasonal"],
+        analysis_year: int,
+    ):
         self.analysis_type = analysis_type
         self.analysis_year = analysis_year
-    
+
     def __str__(self):
         return f"{self.analysis_type}_{self.analysis_year}"
 
-    
+
 electric_utility = Literal["Coned", "National Grid", "NYSEG"]
